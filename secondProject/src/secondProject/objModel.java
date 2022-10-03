b59755bd6ea9a2f7ab521c6129c8471c545f2a2c
twx-a <twx.alfred@gmail.com>
2022-10-03 16:45:02 +0800

    practicing

--- secondProject/src/secondProject/objModel.java
+++ secondProject/src/secondProject/objModel.java
@@ -1,7 +1,32 @@
 package secondProject;
 
 public class objModel {
-	void connected(){
-		System.out.println("Connected to View, Hello from Model");
+
+    private int num; // instance variable
+
+    public objModel(int input) {
+	// instance variable = to input passed
+	// TODO Auto-generated constructor stub
+	this.num = input;
+    }
+
+    void countEachNum() {
+	for (int i = 0; i < num; i++) {
+	    System.out.printf("is %d a prime number? ", i).println(isPrime(i));
 	}
+    }
+
+    public boolean isPrime(int prime) {
+
+	for (int i = 2; i <= prime - 1; i++) {
+	    if (prime % i == 0) {
+		return false;
+	    }
+	}
+	return true;
+    }
+
+    void connected(){
+	System.out.println("Connected to View, Hello from Model");
+    }
 }
--- secondProject/src/secondProject/objView.java
+++ secondProject/src/secondProject/objView.java
@@ -2,11 +2,18 @@ package secondProject;
 
 public class objView {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Init view");
-		objModel myBox = new objModel();
-		myBox.connected();
-	}
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println("Init view");
+	// objModel myBox = new objModel();
+	// myBox.connected();
+	int inputNum = 10;
+	objModel userNum = new objModel(inputNum);
+	userNum.connected();
+	boolean isPrime = userNum.isPrime(inputNum);
+	System.out.printf("is %d a prime number? ", inputNum).println(isPrime);
+	// count up and return
+	userNum.countEachNum();
+    }
 
 }
